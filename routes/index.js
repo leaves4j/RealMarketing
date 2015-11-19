e1ba2d97692ee903e769857a1b41eb307890cae7
jiangq <jiangq.leaves@gmail.com>
2015-11-19 20:31:57 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-var crypto = require("crypto");
-var express=require('express');
-var wechat_config=require('../config/wechat.json');
-var controller=require('../controller');
+var controller = require('../controller');
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
-router.get('/wechat',controller.wechat_check_controller.wechat_check);
-router.post('/wechat',controller.wechat_controller);
+router.get('/wechat', controller.wechat_check_controller.wechat_check);
+router.post('/wechat', function (req, res, next) {
+  console.log(req);
+  next();
+
+}, controller.wechat_controller);
 
 module.exports = router;
