a5d9aa031656830b018dec38857b37fed0501f72
jiangq <jiangq.leaves@gmail.com>
2015-11-19 20:34:29 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,6 @@ router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 router.get('/wechat', controller.wechat_check_controller.wechat_check);
-router.post('/wechat', function (req, res, next) {
-  console.log(req);
-  next();
-
-}, controller.wechat_controller);
+router.post('/wechat', controller.wechat_controller);
 
 module.exports = router;
