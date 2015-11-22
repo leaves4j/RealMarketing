e7e52d5ffb328d7621e56575fb7e7a06c823d2ca
jiangq <jiangq.leaves@gmail.com>
2015-11-22 18:04:23 +0800

    change host

--- controller/sign.js
+++ controller/sign.js
@@ -5,15 +5,15 @@
 "use strict";
 var async = require('async');
 var Sign = require('../model').Sign;
-
+var moment = require('moment');
 module.exports = {
   sign_list: function (req, res, next) {
     var open_id = req.params.open_id;
-    console.log('open_id',open_id)
+    console.log('open_id', open_id)
     async.auto({
       sign_list: function (cb) {
         console.log(1);
-        Sign.find().populate('user').exec(cb)
+        Sign.find().populate('user').sort({'_id': -1}).exec(cb)
       },
       all_sign_count: function (cb) {
         console.log(12);
@@ -30,7 +30,22 @@ module.exports = {
         //next(err)
       }
       else {
-        res.render('sign.dust', results);
+        var person={};
+        var i = 0;
+        var sign_list = results.sign_list.forEach(function (item) {
+          i++;
+
+          item.timestamp = moment(item.timestamp).format('HH:mm:ss');
+          if (item.user.open_id == open_id) {
+            person = item;
+            person.rank = i;
+          }
+          return item;
+        });
+        var render_data=results;
+        render_data.sign_list=sign_list;
+        render_data.person=person;
+        res.render('sign.dust', render_data);
       }
     })
   }
